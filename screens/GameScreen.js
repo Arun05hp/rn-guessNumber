42a1069209fde33aab46ad0d36abcb2a18a533a7
Arun Kumar <kumar05.962013@gmail.com>
2020-06-28 08:34:00 +0530

    Using Dimension Api

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Text, View, StyleSheet, Alert, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Alert,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/NumberContainer";
@@ -104,12 +111,12 @@ const styles = StyleSheet.create({
   wrapper: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: "95%",
   },
   listContainer: {
     flex: 1,
-    width: "60%",
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%",
   },
   list: {
     flexGrow: 1,
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from "react-native";
 
 import Card from "../components/Card";
@@ -113,6 +114,8 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: "80%",
+    maxWidth: "95%",
+    minWidth: 300,
     alignItems: "center",
   },
   buttonContainer: {
@@ -122,7 +125,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   btn: {
-    width: "40%",
+    width: Dimensions.get("window").width / 4,
   },
   input: {
     width: "40%",
